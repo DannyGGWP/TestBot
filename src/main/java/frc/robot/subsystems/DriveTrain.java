09e61f2b71520a4d954b6f99176ebac66522d8d0
KactiKarrot <logan.paliulis@gmail.com>
2022-01-12 06:43:36 -0500

    Added some drive train code

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,11 +4,44 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
+
 public class DriveTrain extends SubsystemBase {
+
+  private final WPI_TalonFX m_motorFR;
+  private final WPI_TalonFX m_motorFL;
+  private final WPI_TalonFX m_motorBR;
+  private final WPI_TalonFX m_motorBL;
+
+  private final MotorControllerGroup m_controllerGroupL;
+  private final MotorControllerGroup m_controllerGroupR;
+
+  private final DifferentialDrive m_differentialDrive;
+
+  public AHRS m_gyro;
+
   /** Creates a new DriveTrain. */
-  public DriveTrain() {}
+  public DriveTrain() {
+    m_motorFR = new WPI_TalonFX(0);
+    m_motorFL = new WPI_TalonFX(1);
+    m_motorBR = new WPI_TalonFX(2);
+    m_motorBL = new WPI_TalonFX(3);
+
+    m_controllerGroupL = new MotorControllerGroup(m_motorFL, m_motorBL);
+    m_controllerGroupR = new MotorControllerGroup(m_motorFR, m_motorBR);
+
+    m_differentialDrive = new DifferentialDrive(m_controllerGroupL, m_controllerGroupR);
+
+    m_gyro = new AHRS(Port.kMXP);
+  }
 
   @Override
   public void periodic() {
